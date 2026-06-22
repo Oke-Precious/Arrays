361b5f57892881cbaeddcb59b536d72ddb290ab7
Oke Precious <okepreciousab@gmail.com>
2026-06-21 21:09:23 -1100

    i added spinner

--- Signup and Signin/script.js
+++ Signup and Signin/script.js
@@ -1,10 +1,15 @@
 
+const allUsers = JSON.parse(localStorage.getItem('myUsers')) || [];
 const showError = (message, errorColor) => {
     const errorMessage = document.getElementById('errorMessage');
     errorMessage.innerHTML = message;
     errorMessage.style.fontSize = '13px';
     errorMessage.style.fontWeight = 'bold';
     errorMessage.style.color = errorColor;
+    errorMessage.style.marginBottom = '10px'
+    setTimeout(() => {
+        errorMessage.innerHTML= '';
+    }, 1500);
 }
 
 const createAccount = () => {
@@ -12,14 +17,50 @@ const createAccount = () => {
     const lastName = document.getElementById('lastName').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-    
-    // alert(firstName);
 
-    if(firstName === '' || lastName === '' || email === '' || password === '') {
+
+    if (firstName === '' || lastName === '' || email === '' || password === '') {
         showError('Please fill in all fields', 'red');
         return;
     }
-    else{
-        showError('Account created successfully', 'green');
+    else {
+        const userObj = {
+            firstName,
+            lastName,
+            email,
+            password
+        }
+        const found = allUsers.find(user => user.email === email)
+        // console.log(found)
+        if (found) {
+            showError('This user already Exist', 'orange');
+        }
+        else {
+            allUsers.push(userObj);
+            localStorage.setItem('myUsers', JSON.stringify(allUsers));
+            btnload.innerHTML = `<div class="loading">
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                <div>`;
+            
+            setTimeout(() => {
+                window.location.href = 'signin.html';
+            }, 1500);
+
+            
+
+        }
+        // console.log(userObj);
+
     }
 }
+// console.log(window)
+console.log(document);
+
+
+
+// .filter
+// .find use condition to look through and return the first matchz
